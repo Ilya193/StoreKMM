60643251a5172bcad7f65f4b6c3a2f4d30c6356a
ilyapc <ilya.komendantov@list.ru>
2024-04-15 22:12:16 +0300

    refactoring

--- androidApp/src/main/java/ru/ikom/storekmm/android/MainActivity.kt
+++ androidApp/src/main/java/ru/ikom/storekmm/android/MainActivity.kt
@@ -3,12 +3,10 @@ package ru.ikom.storekmm.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.animation.EnterTransition
 import androidx.compose.animation.ExitTransition
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.systemBars
-import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
@@ -20,13 +18,13 @@ import androidx.navigation.compose.rememberNavController
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
             StoreKMMTheme {
                 Surface(
                     modifier = Modifier
-                        .fillMaxSize()
-                        .windowInsetsPadding(WindowInsets.systemBars),
+                        .fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Content()
--- androidApp/src/main/java/ru/ikom/storekmm/android/NetworkScreen.kt
+++ androidApp/src/main/java/ru/ikom/storekmm/android/NetworkScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
@@ -53,7 +54,7 @@ fun NetworkScreen(viewModel: NetworkViewModel = koinViewModel()) {
             }
         }
     else {
-        LazyColumn {
+        LazyColumn(modifier = Modifier.systemBarsPadding()) {
             items(uiState.goods, key = { item -> item.id }) {
                 GoodItem(it)
             }
